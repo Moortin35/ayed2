e1e18accb265203446a72370f47ebfd8225a5012
Moortin35 <moortindelgado@gmail.com>
2025-05-14 18:11:16 -0300

    Ejercicio 4, inciso b a medio hacer

--- lab04/ej4/b-checkpal/checkpal.c
+++ lab04/ej4/b-checkpal/checkpal.c
@@ -5,25 +5,35 @@
 
 #include "strfuncs.h"
 
-#define MAX_LENGTH 20
+#define MAX_LENGTH 100
 
 #define SIZEOF_ARRAY(s) (sizeof(s) / sizeof(*s))
 
 int main(void) {
     char user_input[MAX_LENGTH];
-    char ignore_chars[] = {' ', '?', '!', ',', '-', '.'};
-    char *filtered=NULL;
+    //char ignore_chars[] = {' ', '?', '!', ',', '-', '.'};
+    
+    printf("Ingrese un texto (no más de %d símbolos) para verificar palíndromo: \n", MAX_LENGTH);
+    //scanf("%s", user_input);
+    fgets(user_input, sizeof(user_input), stdin);
 
-    printf("Ingrese un texto (no más de %d símbolos) para verificar palíndromo: ", MAX_LENGTH);
-    scanf("%s", user_input);
-    filtered = string_filter(user_input, ignore_chars[0]);
+    //char *filtered=NULL;
+    //char *aux = NULL;
+    //filtered = calloc(string_length(user_input),sizeof(char));
+    //strcpy(filtered, user_input);
+    printf("%s",user_input);
+    /*
     for (unsigned int i=0; i < SIZEOF_ARRAY(ignore_chars); i++) {
-        filtered = string_filter(filtered, ignore_chars[i]);
+        aux = string_filter(filtered, ignore_chars[i]);
+        free(filtered);
+        strcpy(filtered, aux);
+        free(aux);
     }
 
     printf("El texto:\n\n"
             "\"%s\" \n\n"
             "%s un palíndromo.\n\n", user_input, string_is_symmetric(filtered) ? "Es": "NO es");
+    */
     return EXIT_SUCCESS;
 }
 
