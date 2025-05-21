29df20aa23cd80dee7906b840ff52dd2ca657100
Moortin35 <moortindelgado@gmail.com>
2025-05-21 17:39:38 -0300

    Implementación completa del lab4bis, con sus operaciones correspondientes

--- lab04bis/ej3/main.c
+++ lab04bis/ej3/main.c
@@ -12,9 +12,29 @@ typedef Node *List;
  * @brief Construye y devuelve una lista de ejemplo de 3 elementos
  */
 List setup_example() {
-    //
-    // COMPLETAR ACÁ!
-    //
+    int i; //largo de la lista
+    List l; //lista a retornar
+    Node *p; //puntero p para trabajar
+    /*
+    printf("Inserte largo de la lista: "); //Ingreso el largo de la lista
+    scanf("%d", &i);
+    */
+    i = 3;
+    l = malloc(sizeof(Node)); //inicializo la lista
+    p = l; //asigno el puntero p a l para poder iterar   
+    i--; //Primer nodo construído.
+
+    while(i > 0){
+        p->data = i*10;
+        p->next = malloc(sizeof(Node));
+        p = p->next;
+        i--;
+    }
+    p->data = 0;
+    p->next = NULL;
+    p = NULL;
+
+    return l;
 }
 
 /**
@@ -23,9 +43,17 @@ List setup_example() {
  * Precondicion: la lista xs no debe ser vacía
  */
 void append_example(List xs) {
-    //
-    // COMPLETAR ACÁ!
-    //
+
+    Node *p;
+    p = xs;
+
+    while(p->next != NULL){
+        p = p->next;
+    }
+    p->next = malloc(sizeof(Node));
+    p->next->data = 88;
+    p->next->next = NULL;
+
 }
 
 void show_list(List xs) {
--- lab04bis/ej4/main.c
+++ lab04bis/ej4/main.c
@@ -12,9 +12,29 @@ typedef Node *List;
  * @brief Construye y devuelve una lista de ejemplo de 3 elementos
  */
 List setup_example() {
-    //
-    // COMPLETAR ACÁ!
-    //
+    int i; //largo de la lista
+    List l; //lista a retornar
+    Node *p; //puntero p para trabajar
+    /*
+    printf("Inserte largo de la lista: "); //Ingreso el largo de la lista
+    scanf("%d", &i);
+    */
+    i = 3;
+    l = malloc(sizeof(Node)); //inicializo la lista
+    p = l; //asigno el puntero p a l para poder iterar   
+    i--; //Primer nodo construído.
+
+    while(i > 0){
+        p->data = i*10;
+        p->next = malloc(sizeof(Node));
+        p = p->next;
+        i--;
+    }
+    p->data = 0;
+    p->next = NULL;
+    p = NULL;
+
+    return l;
 }
 
 /**
@@ -23,9 +43,11 @@ List setup_example() {
  * Precondicion: la lista xs no debe ser vacía
  */
 List tail_example(List xs) {
-    //
-    // COMPLETAR ACÁ!
-    //
+    Node *p;
+    p = xs;
+    xs = xs->next;
+    free(p);
+    return xs;
 }
 
 void show_list(List xs) {
