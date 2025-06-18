6f457bb4e0372efbc49f6e9a890ad77caa9f1029
Moortin35 <moortindelgado@gmail.com>
2025-06-18 12:27:42 -0300

    Comienzo de implementación del arreglo circular

--- lab05bis/ej6/list.c
+++ lab05bis/ej6/list.c
@@ -18,9 +18,33 @@ list empty(){
     l->size = 0;   
 }
 
-struct _list {
-    elem a[MAX_LENGTH];
-    int start;
-    int size;
-};
+list addl(elem e, list l){
+    assert(l->size < MAX_LENGTH);
+    l->start = (l->start - 1 + MAX_LENGTH) % MAX_LENGTH;
+    l->a[l->start] = e;
+    l->size++;
+    return l;
+}
 
+bool is_empty(list l){
+    return(l->size == 0);
+}
+
+elem head(list l){
+    assert(!is_empty(l));
+    return l->a[l->start];
+}
+
+list tail(list l){
+    assert(!is_empty(l));
+    l->start = (l->start+1) % MAX_LENGTH;
+    l->size--;
+    return l;
+}
+
+list addr(list l, elem e){
+    assert(l->size < MAX_LENGTH);
+    l->a[(l->start+l->size) % MAX_LENGTH] = e;
+    l->size++;
+    return l;
+}
